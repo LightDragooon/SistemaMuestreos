4fe3f3931d6a35bebb3eeea70f8aca1631d8165d
LightDragooon <estebanchavarriacr@gmail.com>
2018-05-21 21:40:42 -0600

    Pantalla Crear Proyecto

---
+++ app/src/main/java/gruporeque/sistemamuestreos/crear_proyecto.java
@@ -0,0 +1,13 @@
+package gruporeque.sistemamuestreos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class crear_proyecto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_proyecto);
+    }
+}
