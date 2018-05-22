de10ebe9a4492f317266a3e4dc2724dbf6e48e87
Jonathan <jonathanco3553@gmail.com>
2018-05-22 10:25:37 -0600

    Se agrega la pantalla crear trabajadores, se cambia el nombre del boton de trabajadores en menuAdministrador

---
+++ app/src/main/java/gruporeque/sistemamuestreos/crear_trabajador.java
@@ -0,0 +1,14 @@
+package gruporeque.sistemamuestreos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class crear_trabajador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_trabajador);
+        getActionBar().hide();
+    }
+}
