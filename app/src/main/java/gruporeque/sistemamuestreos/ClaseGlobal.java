97766f0c62ffc0a6cd23c294d2216f0ac59d5006
Gerardo Gutierrez <gerardogutierrezquiros.gg@gmail.com>
2018-05-24 21:35:44 -0600

    Update listo
    Sorry por el desmadre xD

--- app/src/main/java/gruporeque/sistemamuestreos/ClaseGlobal.java
+++ app/src/main/java/gruporeque/sistemamuestreos/ClaseGlobal.java
@@ -5,6 +5,7 @@ public class ClaseGlobal {
     public static final String HOST_URL = "https://sistemamuestreos.000webhostapp.com/";
 
     public static final String Usuario_Insert = HOST_URL+"Queries/insertar_usuario.php";
+    public static final String Tarea_Insert = HOST_URL+"Queries/insertar_tarea.php";
     public static final String Trabajador_Insert = HOST_URL+"Queries/insertar_operador.php";
     public static final String Trabajadores_Select = HOST_URL+"Queries/seleccionar_trabajadores.php";
     public static final String Usuarios_Select = HOST_URL+"Queries/seleccionar_usuarios.php";
