3fec11f298d38732096da6041a3a62dae42a92f3
Jonathan <jonathanco3553@gmail.com>
2018-06-03 18:42:27 -0600

    ya inicia sesion

--- app/src/main/java/gruporeque/sistemamuestreos/ClaseGlobal.java
+++ app/src/main/java/gruporeque/sistemamuestreos/ClaseGlobal.java
@@ -4,13 +4,14 @@ public class ClaseGlobal {
 
     public static final String HOST_URL = "https://proyecto2reque.000webhostapp.com/";
 
-    public static final String Usuario_Insert = HOST_URL+"Queries/insertar_usuario.php";
-    public static final String Tarea_Insert = HOST_URL+"Queries/insertar_tarea.php";
-    public static final String Trabajador_Insert = HOST_URL+"Queries/insertar_operador.php";
-    public static final String Trabajadores_Select = HOST_URL+"Queries/seleccionar_trabajadores.php";
-    public static final String Usuarios_Select = HOST_URL+"Queries/seleccionar_usuarios.php";
-    public static final String Eliminar_Usuario = HOST_URL+"Queries/eliminar_usuario.php";
-    public static final String Proyectos_Select = HOST_URL+"Queries/seleccionar_proyectos.php";
+    public static final String Usuario_Insert = HOST_URL + "Queries/insertar_usuario.php";
+    public static final String Tarea_Insert = HOST_URL + "Queries/insertar_tarea.php";
+    public static final String Trabajador_Insert = HOST_URL + "Queries/insertar_operador.php";
+    public static final String Trabajadores_Select = HOST_URL + "Queries/seleccionar_trabajadores.php";
+    public static final String Usuarios_Select = HOST_URL + "Queries/seleccionar_usuarios.php";
+    public static final String Eliminar_Usuario = HOST_URL + "Queries/eliminar_usuario.php";
+    public static final String Proyectos_Select = HOST_URL + "Queries/seleccionar_proyectos.php";
     public static final String Eliminar_Trabajador = HOST_URL + "Queries/eliminar_trabajador.php";
     public static final String Modificar_Trabajador = HOST_URL + "Queries/modificar_trabajador.php";
+    public static final String Iniciar_Sesion = HOST_URL + "Queries/iniciar_sesion.php";
 }
